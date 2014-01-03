93f9b16b27e89e4ec849e0e0bec7d9c121925d37
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-03 15:54:20 +0900

    Initial Commit

---
+++ p91Tests/p91Tests.m
@@ -0,0 +1,34 @@
+//
+//  p91Tests.m
+//  p91Tests
+//
+//  Created by SDT-1 on 2014. 1. 3..
+//  Copyright (c) 2014년 SDT-1. All rights reserved.
+//
+
+#import <XCTest/XCTest.h>
+
+@interface p91Tests : XCTestCase
+
+@end
+
+@implementation p91Tests
+
+- (void)setUp
+{
+    [super setUp];
+    // Put setup code here. This method is called before the invocation of each test method in the class.
+}
+
+- (void)tearDown
+{
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+}
+
+@end
