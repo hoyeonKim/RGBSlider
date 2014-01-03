200a4c7868ac6c700db94037893e63061704fa8a
hoyeonKim <roadmaker@korea.ac.kr>
2014-01-03 16:32:41 +0900

    Slider
    완료했습니다.

--- p91/ViewController.m
+++ p91/ViewController.m
@@ -10,10 +10,42 @@
 
 @interface ViewController ()
 
+@property (weak, nonatomic) IBOutlet UIView *view1;
+@property (weak, nonatomic) IBOutlet UISlider *sl1;
+@property (weak, nonatomic) IBOutlet UISlider *sl2;
+@property (weak, nonatomic) IBOutlet UISlider *sl3;
+@property (weak, nonatomic) IBOutlet UIActivityIndicatorView *indi;
+
 @end
 
 @implementation ViewController
-
+- (IBAction)sliderChanged:(id)sender {
+    if([_indi isAnimating]){
+        [_indi stopAnimating];
+    }
+    
+    _view1.backgroundColor=[UIColor colorWithRed:_sl1.value green:_sl2.value blue:_sl3.value alpha:1];
+}
+- (IBAction)power:(id)sender {
+    BOOL isOn = ((UISwitch *)sender).on;
+    _sl1.enabled = isOn;
+    _sl2.enabled = isOn;
+    _sl3.enabled = isOn;
+    
+    if(isOn)
+    {
+        [_indi startAnimating];
+        [self performSelector:@selector(sliderChanged:) withObject:nil afterDelay:1.0];
+        
+    }
+    else{
+        self.view1.backgroundColor=[UIColor grayColor];
+    }
+    
+}
+-(void) viewWillAppear:(BOOL)animated{
+    [self sliderChanged:nil];
+}
 - (void)viewDidLoad
 {
     [super viewDidLoad];
